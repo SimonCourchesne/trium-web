2011d2e49804ef8d9a52cff05768b8e6356686cb
Simon Courchesne <simon.courchesne@mail.mcgill.ca>
2026-02-18 12:51:35 -0500

    fixed header visibility issue

--- components/header.tsx
+++ components/header.tsx
@@ -22,7 +22,7 @@ export function Header() {
     return (
         <header
             className={cn(
-                "fixed top-0 left-0 right-0 z-50 transition-all duration-300",
+                "fixed top-0 left-0 right-0 z-[100] transition-all duration-300",
                 isScrolled ? "py-4" : "py-6"
             )}
         >
